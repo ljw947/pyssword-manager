64463d0c96774ebfb4170967611625a74545e47b
Lachlan Watson <lachlanjwatson@gmail.com>
2023-11-22 20:41:19 +1000

    Stubbed out functionality

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+"""Simple CLI password manager tool."""
+
+
+import sys
+
+
+def add_password() -> None:
+    print("add password")
+
+
+def search_password() -> None:
+    print("search password")
+
+
+def delete_password() -> None:
+    print("delete password")
+
+
+def list_password() -> None:
+    print("list password")
+
+
+def help() -> None:
+    print("Pyssword Manager")
+    print("Available options:")
+    print("     add / a:        add a password")
+    print("     delete / d:     delete a password")
+    print("     list / l:       list available information")
+    print("     search / s:     search for password information")
+    print("     help / h:       list this help menu")
+
+
+def handle_command(command: str) -> None:
+    """Handle command"""
+    lower_command = command.lower()
+    if lower_command in ("exit", "quit", "q"):
+        print("Exiting.")
+        sys.exit(0)
+
+    if lower_command in ("add", "a"):
+        add_password()
+        return
+
+    if lower_command in ("search", "s"):
+        search_password()
+        return
+
+    if lower_command in ("delete", "d"):
+        delete_password()
+        return
+
+    if lower_command in ("list", "l"):
+        list_password()
+        return
+
+    if lower_command in ("help", "h"):
+        help()
+        return
+
+    print("Command not recognised. Type help or h for help menu.")
+    return
+
+
+def main():
+    print("Pyssword Manager")
+    while True:
+        command = input("Enter command: ")
+        handle_command(command)
+
+
+if __name__ == "__main__":
+    main()
