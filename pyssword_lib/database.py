54340f7122300895e88963f8ef5b32537afc9c29
Lachlan Watson <lachlanjwatson@gmail.com>
2024-03-09 21:32:00 +1000

    Added arg to Database for path to database storage

--- pyssword_lib/database.py
+++ pyssword_lib/database.py
@@ -15,34 +15,35 @@ class Database():
     Base class representing a sqlite3 database object.
     Derived classes will extend it when instantiated.
     """
-    def __init__(self):
+    def __init__(self, database_dir: Path = DEFAULT_DATABASE_PATH):
+        self.database_dir = database_dir
         self.database_name = None
-        self.database_path = None
+        self.database_file_path = None
         self.sqlite_connection = None
 
     def check_database(self) -> bool:
         """Check if given database is initialised. If it is, try and unlock it."""
-        if not Path.exists(self.database_path):
+        if not Path.exists(self.database_file_path):
             logging.debug("%s database does not exist.", self.database_name)
             return False
         return True
 
     def setup_database_path(self) -> None:
         """Helper method to set up path to database file."""
-        self.database_path = DEFAULT_DATABASE_PATH / f"{self.database_name}.db"
+        self.database_file_path = self.database_dir / f"{self.database_name}.db"
 
     def create_database_path(self) -> None:
         """Create the database on disk if not already created."""
-        if not Path.exists(DEFAULT_DATABASE_PATH):
-            Path.mkdir(DEFAULT_DATABASE_PATH, parents=True)
+        if not Path.exists(self.database_dir):
+            Path.mkdir(self.database_dir, parents=True)
 
     def initialise_database_file(self) -> sqlite3.Connection:
         """Initialise database for usage."""
         try:
             self.setup_database_path()
             self.create_database_path()
-            connection = sqlite3.connect(self.database_path)
+            connection = sqlite3.connect(self.database_file_path)
         except Exception as error:  # TODO: no broad except
-            logging.error(f"Unable to create database storage at {DEFAULT_DATABASE_PATH}, %s.", error)
+            logging.error(f"Unable to create database storage at {self.database_dir}, %s.", error)
 
         self.sqlite_connection = connection
